e6add6432e09743fc14d350ef66c4d7553720daf
D4N13L98 <dancastell1022@gmail.com>
2022-08-26 23:48:30 -0500

    Clase Empleado agrgada al repositorio EntregaSprint2

---
+++ ClaseEmpleado/ClaseEmpleado/src/test/java/com/ScrumSprinters/ClaseEmpleado/ClaseEmpleadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ScrumSprinters.ClaseEmpleado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaseEmpleadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
