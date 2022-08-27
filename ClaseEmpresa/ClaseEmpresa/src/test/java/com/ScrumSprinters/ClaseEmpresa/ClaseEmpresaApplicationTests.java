465fffc9dd63159e1d71fa2641ee7d3056b99189
fdbarrios8 <111377502+fdbarrios8@users.noreply.github.com>
2022-08-26 21:31:09 -0500

    Clase empresa
    Clase empresa

---
+++ ClaseEmpresa/ClaseEmpresa/src/test/java/com/ScrumSprinters/ClaseEmpresa/ClaseEmpresaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ScrumSprinters.ClaseEmpresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaseEmpresaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
